b43f6e8a7ffd385089cce18e50c8f5ba497020d7
imtushar01 <iamtusharprajapati@gmail.com>
2026-08-05 15:53:02 +0530

    SOLID-Learnings

---
+++ 02. SOLID Principles/03. LSP/LSP_followed.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class DepositOnlyAccount {
+public:
+    virtual void deposit(double amount) = 0;
+};
+
+class WithdrawableAccount : public DepositOnlyAccount {
+public:
+    virtual void withdraw(double amount) = 0;
+};
+
+class SavingAccount: public WithdrawableAccount {
+private:
+    double balance;
+
+public:
+    SavingAccount(){
+        balance = 0;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: " << amount << " in Savings Account. New Balance: " << balance << endl;
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            cout << "Withdrawn: " << amount << " from Savings Account. New Balance: " << balance << endl;
+        } else {
+            cout << "Insufficient funds in Savings Account!\n";
+        }
+    }
+};
+
+class CurrentAccount: public WithdrawableAccount {
+private:
+    double balance;
+
+public:
+    CurrentAccount(){
+        balance = 0;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: " << amount << " in Current Account. New Balance: " << balance << endl;
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            cout << "Withdrawn: " << amount << " from Current Account. New Balance: " << balance << endl;
+        } else {
+            cout << "Insufficient funds in Current Account!\n";
+        }
+    }
+
+};
+
+class FixedTermAccount : public DepositOnlyAccount {
+private:
+    double balance;
+
+public:
+    FixedTermAccount() { 
+        balance = 0; 
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: " << amount << " in Fixed Term Account. New Balance: " << balance << endl;
+    }
+};
+
+class BankClient {
+private:
+    vector<WithdrawableAccount*> withdrawableAccounts;
+    vector<DepositOnlyAccount*> depositOnlyAccounts;
+
+public:
+    BankClient( vector<WithdrawableAccount*> withdrawableAccounts, vector<DepositOnlyAccount*> depositOnlyAccounts) { 
+        this->withdrawableAccounts = withdrawableAccounts; 
+        this->depositOnlyAccounts = depositOnlyAccounts;
+    }
+
+    void processTransactions() {
+        for (WithdrawableAccount* acc : withdrawableAccounts) {
+            acc->deposit(1000);
+            acc->withdraw(500); 
+        }
+        for (DepositOnlyAccount* acc : depositOnlyAccounts) {
+            acc->deposit(5000);
+        }
+    }
+};
+
+int main(){
+    vector<WithdrawableAccount*> WithdrawableAccounts;
+    WithdrawableAccounts.push_back(new SavingAccount());
+    WithdrawableAccounts.push_back(new CurrentAccount());
+
+    vector<DepositOnlyAccount*> DepositOnlyAccounts;
+    DepositOnlyAccounts.push_back(new FixedTermAccount());
+
+    BankClient* client = new BankClient(WithdrawableAccounts, DepositOnlyAccounts);
+    client->processTransactions();
+
+    return 0;
+}
